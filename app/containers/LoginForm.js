74e64187a3857edeb1a51f9a8927ea645baa365a
David <david@fabacus.com>
2017-10-04 15:43:29 +0100

    login with background image and rounded buttons

--- app/containers/LoginForm.js
+++ app/containers/LoginForm.js
@@ -4,12 +4,13 @@ import React, { Component } from 'react';
 import PouchDB from 'pouchdb-react-native';
 import { Actions } from 'react-native-router-flux';
 import { Col, Row, Grid } from 'react-native-easy-grid';
-import { Container, Content, Form, Item, Input, Button, Label, Spinner } from 'native-base';
-import { Text, AsyncStorage, Image, Alert } from 'react-native';
+import { Container, Content, Form, Item, Input, Label, Spinner, Button } from 'native-base';
+import { Text, AsyncStorage, Image, Alert, Dimensions, View } from 'react-native';
 import { connect } from 'react-redux';
-// import BaseViewScreen from '../BaseViewScreen';
+import TextFieldLogin from '../containers/common/TextFieldLogin';
 
-// PouchDB.plugin(require('pouchdb-find'));
+const fullWidth = Dimensions.get('window').width; // full width
+const fullHeight = Dimensions.get('window').height; // full height
 
 class LoginForm extends Component {
 
@@ -96,90 +97,55 @@ class LoginForm extends Component {
 		}
 	}
 
-	renderLoginButton() {
-		return (
-			<Row style={{ marginTop: 10 }}>
-				<Col>
-					<Button onPress={this.login.bind(this)} block info>
-					<Text>Login</Text>
-					</Button>
-				</Col>
-			</Row>
-		);
-	}
-
-	renderCreateNewCompanyTextBox() {
-		if (this.state.checkedNewCompany) {
-			return (
-				<Item floatingLabel>
-					<Label>Company name</Label>
-						<Input
-						onChangeText={(text) => {
-							this.onChangeText(text, 'companyName');
-						}}
-						value={this.company.companyName}
-						/>
-				</Item>
-			);
-		}
-		return (null);
-	}
-
-	renderRegisterButton() {
-		return (
-			<Row style={{ marginTop: 22 }}>
-				<Col>
-					<Button onPress={this.register.bind(this)} block info>
-					<Text>Register</Text>
-					</Button>
-				</Col>
-			</Row>
-		);
-	}
-
 	render() {
 		return (
-			// source={require('../img/symphonylogo.png')}
-			// <BaseViewScreen>
-				<Content style={{ padding: 22 }}>
-					<Grid>
-						<Row>
-							<Image
-							style={{ width: 250, height: 75 }}
-
-							/>
-						</Row>
-						<Row>
-							<Col>
-								<Form>
-									{this.renderCreateNewCompanyTextBox()}
-									<Item floatingLabel>
-										<Label>Username</Label>
-											<Input
-											onChangeText={(text) => {
-												this.onChangeText(text, 'firebaseEmail');
-											}}
-											value={this.state.firebaseUser.firebaseEmail}
-											/>
-									</Item>
-									<Item floatingLabel last>
-										<Label>Password</Label>
-											<Input
-												secureTextEntry
-												onChangeText={(text) => {
-													this.onChangeText(text, 'firebasePassword');
-												}}
-												value={this.state.firebaseUser.firebasePassword}
-											/>
-									</Item>
-								</Form>
-							</Col>
-						</Row>
-						{this.renderLoginButton()}
-						{this.renderRegisterButton()}
-					</Grid>
-				</Content>
-			// </BaseViewScreen>
+			<View style={{ flex: 1 }}>
+				<Image source={require('../img/loginbackground.png')} style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center', width: null, height: null }} >
+					<View style={{ backgroundColor: 'transparent', alignItems: 'center', }}>
+						<TextFieldLogin
+							placeholder="Email"
+							style={{ marginTop: 50, paddingLeft: 10, paddingRight: 10, backgroundColor: 'white', textAlign: 'center', width: fullWidth - 100, alignSelf: 'center', borderWidth: 2, borderColor: 'steelblue' }}
+							label=""
+							value={this.state.firebaseUser.firebaseEmail}
+							onChangeText={value => this.onChangeText(value, 'firebaseEmail')} />
+						<TextFieldLogin
+							secureTextEntry
+							placeholder="Password"
+							style={{ marginTop: 90, paddingLeft: 10, paddingRight: 10, backgroundColor: 'white', textAlign: 'center', width: fullWidth - 100, alignSelf: 'center', borderWidth: 2, borderColor: 'steelblue' }}
+							label=""
+							value={this.state.firebaseUser.firebasePassword}
+							onChangeText={value => this.onChangeText(value, 'firebasePassword')} />
+						<Button
+							small
+							rounded
+							style={{
+								backgroundColor: 'steelblue',
+								borderWidth: 2,
+								borderColor: 'steelblue',
+								alignSelf: 'center',
+								marginTop: 150
+							}}
+							onPress={this.login.bind(this)}
+						>
+							<Text>&nbsp;&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+						</Button>
+						<Button
+							small
+							rounded
+							style={{
+								backgroundColor: '#9DBDF2',
+								borderWidth: 1,
+								borderColor: 'steelblue',
+								alignSelf: 'center',
+								marginTop: 20
+							}}
+							onPress={this.register.bind(this)}
+						>
+							<Text>&nbsp;&nbsp;&nbsp;Register&nbsp;&nbsp;&nbsp;</Text>
+						</Button>
+					</View>
+        		</Image>
+     		</View>
 		);
 	}
 }
---
+++ app/containers/common/TextFieldLogin.js
@@ -0,0 +1,67 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { View, TextInput } from 'react-native';
+import { Text } from 'native-base';
+
+import MIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { styles } from '../../Styles/general';
+
+class TextField extends Component {
+	static propTypes = {
+		value: PropTypes.string.isRequired,
+		label: PropTypes.string,
+		validate: PropTypes.bool,
+		height: PropTypes.number,
+	}
+
+	static defaultProps = {
+		value: '',
+		label: 'Label',
+		validate: false,
+		height: 35
+	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			TextValue: this.props.value,
+			label: this.props.label,
+			validate: this.props.validate,
+			height: this.props.height
+		};
+	}
+
+	componentWillMount() {
+		this.setState({ TextValue: this.props.value });
+		this.setState({ label: this.props.label });
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.value !== this.props.value) {
+			this.setState({ TextValue: nextProps.value });
+		}
+	}
+
+	render() {
+		return (
+			<View style={{ marginBottom: 10, height: this.state.height + 20, flex: 1, marginRight: 2 }}>
+				{this.state.validate && <Text style={this.state.TextValue === '' ? styles.textError : styles.textSuccess}>{this.state.label}</Text>}
+				{!this.state.validate && <Text style={styles.textLabel}>{this.state.label}</Text>}
+				<View style={{ ...styles.textInputLogin, height: this.state.height }}>
+				<TextInput style={{ ...styles.textInputNB, height: this.state.height }} {...this.props} autoCorrect={false} returnKeyType="done" underlineColorAndroid={'transparent'} />
+					{this.state.validate && <View>
+						<MIcons name={this.state.TextValue === '' ? 'close-circle-outline' : 'check-circle-outline'} size={20} style={this.state.TextValue === '' ? styles.colError : styles.colSuccess} />
+					</View>}
+				</View>
+			</View>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return state;
+};
+
+export default connect(mapStateToProps, { })(TextField);
